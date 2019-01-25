10160216ca2a7d6c16769798499b6bb4169af494
Paul Rosenberg <paulrosenberg13@gmail.com>
2019-01-25 14:53:08 -0800

    added run and close server

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
+'use strict';
+
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+mongoose.Promise = global.Promise;
+
+const { PORT } = require('./config');
+
 const app = express();
 
+//Static webserver
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+//Parse request body
+app.use(express.json());
+
+/*TODO:
+- Add Routes && Protected rputes
+- Add custom error handler
+*/
+
+let server;
+
+function runServer() {}
+
+function closeServer() {}
+
